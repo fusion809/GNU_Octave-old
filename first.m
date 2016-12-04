9752f0c6b321183c0a95dc04f6dc01a54438c9ca
fusion809 <brentonhorne77@gmail.com>
2016-12-05 06:00:24 +1000

    Updating

--- first.m
+++ first.m
@@ -1,66 +1,114 @@
 # Brenton Horne's script 20161204
 clear all
 
-# constants
+# Constants
+## N for Chebyshev extrema grid
 N         = 1000;
+## NN for interpolation linspace grid
 NN        = 100000;
-n         = 0:N;
+## [a,b] is the problem interval
 a         = 0;
 b         = 200;
+## - d2Y/dx2 + kxY = lambda Y is the problem
 k         = 1;
-M         = round(N/6);
-nsub      = n(2:N);
+## How far we are computing the root-mean square error up to
+M         = round(N/3);
 format long g;
 
-# Basic matrices
+###################################################################################################
+# Basic matrices                                                                                  #
+###################################################################################################
+
+## Vectors
+### n vector: a row vector of elements from 0 to N
+n         = 0:N;
+### nsub vector: the n vector without 0 and N
+nsub      = n(2:N);
+### Chebyshev extrema grid
 x         = -cos(pi*n'/N);
-xx        = linspace(-1,1,NN+1)';
-yy        = (b-a)/2*xx+(a+b)/2;
+### grid without endpoints
 xsub      = x(2:N);
+### map Chebyshev extrema grid to integration interval: [a,b]
 y         = (b-a)/2*x+(a+b)/2;
+### y without endpoints a and b
 ysub      = (b-a)/2*xsub+(a+b)/2;
+### Linearly-spaced grid on [-1,1] for later interpolation
+xx        = linspace(-1,1,NN+1)';
+### map interpolation grid to integration interval: [a,b]
+yy        = (b-a)/2*xx+(a+b)/2;
+
+###################################################################################################
+
+## More square-shaped matrices
+### Chebyshev T(x) matrix for extrema grid
 T         = cos(acos(x)*n);
-TT        = cos(acos(xx)*n);
+### T without x endpoints
 Tsub      = T(2:N,:);
+### Chebyshev matrix for linearly-spaced interpolation grid
+TT        = cos(acos(xx)*n);
+### Chebyshev U(x) matrix for extrema grid without endpoints (singularities exist there)
 Usub      = diag(1./sqrt(1-xsub.^2))*sin(acos(xsub)*n);
+### First derivative of the Chebyshev T(x) function, on the extrema grid without endpoints
 dTsub     = Usub*diag(n);
+### dT with endpoints
 dT        = [-((-1).^n).*n.^2; dTsub; n.^2];
+### Second derivative of T(x) on extrema grid without endpoints
 d2Tsub    = diag(1./(1-xsub.^2))*(diag(xsub)*Usub-Tsub*diag(n))*diag(n);
+### Second derivative of T(x) on extrema grid with endpoints
 d2T       = [((-1).^n).*(n.^2).*(n.^2-1)/3; d2Tsub; (n.^2).*(n.^2-1)/3];
+### Second-order differentiation matrix for extrema grid
 D2        = d2T/T;
+### Second-order differentiation matrix for extrema grid without endpoints
 E2        = D2(2:N,2:N);
 
-# Computation
-# H is the matrix version of the Sturm-Liouville operator.
-H         = - 4/((b-a)^2)*E2 + k*diag(ysub);
-[Y, LAM]  = eig(H);
-# Convert LAM (diagonal matrix) to vertical vector.
-Lam       = diag(LAM);
-[Lam, IX] = sort(Lam, 'ascend');
-Y         = Y(:,IX);
+###################################################################################################
+# Computation                                                                                     #
+###################################################################################################
 
-# Add boundary values
+## H is the matrix version of the Sturm-Liouville operator.
+## 4/(b-a)^2*E2 is the second-order differentiation matrix on the mapped interval
+H         = - 4/((b-a)^2)*E2 + k*diag(ysub);
+## Solution to eigenvalue problem H Y = Lam Y
+[Y, LAM]  = eig(H);
+## Convert LAM (diagonal matrix) to vertical vector.
+Lam       = diag(LAM);
+## Order Lam in ascending order
+[Lam, IX] = sort(Lam, 'ascend');
+## Order Y in the same order
+Y         = Y(:,IX);
+## Add boundary values
 Y         = [zeros(1,N-1); Y; zeros(1,N-1)];
 
-# Interpolate
+###################################################################################################
+# Interpolate                                                                                     #
+###################################################################################################
+
+## Determine expansion coefficients for Chebyshev series
 aa        = T\Y;
+
+## Interpolate to linear grid
 YY        = TT*aa;
 
 # Error analysis
-# the exact solution calls for the eigenvalues to negative the zeros of the Airy Ai function
+## the exact solution calls for the eigenvalues to negative the zeros of the Airy Ai function
 err       = airy(0,-Lam);
+## root-mean square error for err vector up to element M
 rms       = sqrt(sum(err(1:M).^2)/(M));
 
 # Plotting
+## Plot the log of err
 figure(1);
 plot(nsub,log10(err),"linewidth",2)
 title("Log10 of Airy Ai of - the eigenvalues; lower (more negative) the better the solution")
+## plot the first eigenfunction
 figure(2);
 plot(yy,YY(:,1),'-r',"linewidth",2)
 title("Plot of the first eigenfunction on [0,200]")
+## plot the 150th eigenfunction
 figure(3);
 plot(yy,YY(:,150),'-g',"linewidth",2)
 title("Plot of the 150th eigenfunction on [0,200]")
+## plot the 300th eigenfunction
 figure(4);
 plot(yy,YY(:,300),'-m',"linewidth",2)
 title("Plot of the 300th eigenfunction on [0,200]")
