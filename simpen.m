44d1d41c58a883ec4d466aeb8b8244dce317b65d
Brenton Horne <brentonhorne77@gmail.com>
2017-01-04 08:11:24 +1000

    Updating

--- simpen.m
+++ simpen.m
@@ -1,5 +1,6 @@
 function dy = simpen(y,t)
-dy = zeros(2,1);
-g=9.8; l=1;
-dy(1) = y(2);
-dy(2) = -g/l*cos(y(1));
+  dy    = zeros(2,1);
+  g     = 9.8;
+  l     = 1;
+  dy(1) = y(2);
+  dy(2) = -g/l*cos(y(1));
